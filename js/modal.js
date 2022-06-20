a07e60a094ce54c16542d35abb0386c2ee68796f
Artem Rudzevych <mrbig8644@gmail.com>
2022-06-20 13:47:48 +0300

    создал форму

--- js/modal.js
+++ js/modal.js
@@ -14,5 +14,19 @@ function toggleModal() {
 }
 
 function logBackdropClick() {
-  console.log("Это клик в бекдроп");
+  console.log("Отправка формы");
 }
+// Form
+
+(() => {document
+  .querySelector('.js-speaker-form')
+  .addEventListener('submit', e => {
+  e.preventDefault();
+  
+  new FormData(e.currentTarget).forEach((value, name) => 
+  console.log(`${name}: ${value}`),
+  
+  );
+  
+  });
+  })();
